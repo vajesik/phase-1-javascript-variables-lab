3d89ed1f198df815444abdf91a2eb8506a6ea1c4
Vanessa Jesik <vajesik@gmail.com>
2023-07-31 10:21:29 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+const companyName = "Scuber"
+
+let mostProfitableNeighborhood
+mostProfitableNeighborhood = "Chelsea"
+
+let companyCeo
+companyCeo = "Susan Smith"
